d488166fd6666255e684a33b4a7b8afb95fb7e53
Kevin Nathanael Limarga <krenos@Kevins-MacBook-Air.local>
2026-05-13 22:16:55 +0700

    chore: add database schema & update admin queries

---
+++ netspace.sql
@@ -0,0 +1,52 @@
+-- CREATE DATABASE Netspace;
+
+CREATE TABLE Locations (
+	id SERIAL PRIMARY KEY,
+	slug VARCHAR(55) UNIQUE NOT NULL,
+	name VARCHAR(100) UNIQUE NOT NULL,
+	isActive BOOL DEFAULT FALSE
+);
+
+CREATE TABLE Users (
+	id VARCHAR(10) UNIQUE NOT NULL,
+	name VARCHAR(30),
+	age INT,
+	gender VARCHAR(6)
+);
+
+CREATE TABLE Interests (
+	id SERIAL PRIMARY KEY,
+	emoji TEXT NOT NULL,
+	label TEXT NOT NULL
+);
+
+CREATE TABLE UserInterests (
+	userId VARCHAR(10) REFERENCES Users(id),
+	interestId INT REFERENCES Interests(id),
+	PRIMARY KEY(userId, interestId)
+);
+
+CREATE TABLE UserCustomInterests (
+	userId VARCHAR(10) REFERENCES Users(id),
+	emoji TEXT NOT NULL,
+	label TEXT NOT NULL,
+	PRIMARY KEY(userId, label)
+);
+
+INSERT INTO Interests (emoji, label)
+VALUES ('☕', 'Kopi'),
+  ('🎮', 'Gaming'),
+  ('📚', 'Buku'),
+  ('🎵', 'Musik'),
+  ('🍜', 'Kuliner'),
+  ('✈️', 'Travel'),
+  ('💻', 'Tech'),
+  ('🎨', 'Seni'),
+  ('🏋️', 'Olahraga'),
+  ('🎬', 'Film'),
+  ('📷', 'Fotografi'),
+  ('🌱', 'Tanaman');
+
+SELECT * FROM Interests;
+
+SELECT * FROM Users;
